49ad5dc841050bc2392bbf975b7637b5d3a7f52b
gandaraipower <jjangjongsoo@naver.com>
2025-09-02 12:44:11 +0900

    14강

--- src/main/java/com/spring3/domain/post/post/controller/PostController.java
+++ src/main/java/com/spring3/domain/post/post/controller/PostController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.stream.Collectors;
+
 @Controller
 public class PostController {
 
@@ -71,14 +73,17 @@ public class PostController {
     public String doWrite(@Valid PostWriteForm form, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            FieldError fieldError = bindingResult.getFieldError();
-            String fieldName = fieldError.getField();
-            String errorMessage = fieldError.getDefaultMessage();
 
-            System.out.println("fieldName :" + fieldName);
-            System.out.println("errorMessage :" + errorMessage);
+            String fieldName = "title";
 
-            return getWriteFormHtml(errorMessage, form.title, form.content, fieldName);
+            String errorMessages=bindingResult.getFieldErrors()
+                            .stream()
+                                    .map(FieldError::getDefaultMessage)
+                                            .collect(Collectors.joining("<br>"));
+
+
+
+            return getWriteFormHtml(errorMessages, form.title, form.content, fieldName);
         }
 
 //        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.", title, content, "title");
