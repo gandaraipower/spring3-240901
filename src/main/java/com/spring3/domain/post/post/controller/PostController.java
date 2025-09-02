6e43378ecb549b469ba9fc8c4533e05bed360c4d
gandaraipower <jjangjongsoo@naver.com>
2025-09-02 11:12:40 +0900

    8강

--- src/main/java/com/spring3/domain/post/post/controller/PostController.java
+++ src/main/java/com/spring3/domain/post/post/controller/PostController.java
@@ -16,24 +16,33 @@ public class PostController {
         this.postService = postService;
     }
 
-    private String getWriteFormHtml(String errorMessage,String title,String content){
+    private String getWriteFormHtml(String errorMessage, String title, String content, String errorFieldName) {
         return """
                 <div style="color:red">%s</div>
+                
                 <form method="POST" action="/posts/doWrite">
-                  <input type="text" name="title" value="%s" autoFocus="true">
+                  <input type="text" name="title" value="%s" autoFocus>
                   <br>
-                  <textarea name="content" value="%s"></textarea>
+                  <textarea name="content">%s</textarea>
                   <br>
                   <input type="submit" value="작성">
                 </form>
-                """.formatted(errorMessage,title,content);
+                
+                <script>
+                    const errorFieldName = "%s";
+                    
+                    if(errorFieldName.length > 0) {
+                        const form = document.querySelector("form");
+                        form[errorFieldName].focus();
+                    }
+                </script>
+                """.formatted(errorMessage, title, content, errorFieldName);
     }
 
     @GetMapping("/posts/write")
     @ResponseBody
     public String write() {
-
-        return getWriteFormHtml("","","");
+        return getWriteFormHtml("", "", "", "");
     }
 
     @PostMapping("/posts/doWrite")
@@ -43,9 +52,8 @@ public class PostController {
             String content
     ) {
 
-        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.",title,content);
-        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요",title,content);
-
+        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.", title, content, "title");
+        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.", title, content, "content");
 
         Post post = postService.write(title, content);
 
