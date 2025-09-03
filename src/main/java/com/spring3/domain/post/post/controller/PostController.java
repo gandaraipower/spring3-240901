bfae8eb90032ded7292a1dee57b154aaf2d9f6f8
gandaraipower <jjangjongsoo@naver.com>
2025-09-03 11:22:29 +0900

    25강

--- src/main/java/com/spring3/domain/post/post/controller/PostController.java
+++ src/main/java/com/spring3/domain/post/post/controller/PostController.java
@@ -26,12 +26,12 @@ public class PostController {
     @AllArgsConstructor
     @Getter
     public static class PostWriteForm {
-        @NotBlank(message = "1-제목을 입력해주세요.")
-        @Size(min = 2, max = 10, message = "2-제목은 2글자 이상 10글자 이하로 입력해주세요.")
+        @NotBlank(message = "01-title-제목을 입력해주세요.")
+        @Size(min = 2, max = 10, message = "02-title-제목은 2글자 이상 10글자 이하로 입력해주세요.")
         private String title;
 
-        @NotBlank(message = "3-내용을 입력해주세요.")
-        @Size(min = 2, max = 100, message = "4-내용은 2글자 이상 100글자 이하로 입력해주세요.")
+        @NotBlank(message = "03-content-내용을 입력해주세요.")
+        @Size(min = 2, max = 100, message = "04-content-내용은 2글자 이상 100글자 이하로 입력해주세요.")
         private String content;
     }
 
